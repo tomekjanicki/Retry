b37a007134f75cbef0b751c17536dabd3f7df98f
Tomasz Janicki <tomasz.janicki@inetum.com>
2023-07-21 12:57:19 +0200

    fix

--- Retry/Services/WithRetryExternalApiClient.cs
+++ Retry/Services/WithRetryExternalApiClient.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Extensions.Options;
 using OneOf;
 using OneOf.Types;
-using Polly;
 using Retry.Extensions;
 
 namespace Retry.Services;
@@ -9,44 +8,47 @@ namespace Retry.Services;
 public sealed class WithRetryExternalApiClient : IExternalApiClient
 {
     private readonly IExternalApiClient _api;
-    private readonly IAsyncPolicy<string> _getTimeAsStringPolicy;
-    private readonly IAsyncPolicy<IReadOnlyCollection<string>> _getItemsPolicy;
-    private readonly IAsyncPolicy<OneOf<string, NotFound, Error>> _getUserFullNameByIdPolicy;
-    private readonly Handlers<string> _getTimeAsStringHandler;
-    private readonly Handlers<IReadOnlyCollection<string>> _getItemsHandler;
-    private readonly Handlers<OneOf<string, NotFound, Error>> _getUserFullNameByIdHandler;
+    private readonly PolicyAndHandlerWrapper<string> _getTimeAsStringPolicyAndHandler;
+    private readonly PolicyAndHandlerWrapper<IReadOnlyCollection<string>> _getItemsPolicyAndHandler;
+    private readonly PolicyAndHandlerWrapper<OneOf<string, NotFound, Error>> _getUserFullNameByIdPolicyAndHandler;
 
     public WithRetryExternalApiClient(IExternalApiClient api, IOptions<ConfigurationSettings> options, ILogger<WithRetryExternalApiClient> logger)
     {
         _api = api;
         var configuration = options.Value.RetryAndCircuitBreakerPolicyConfiguration;
-        _getTimeAsStringHandler = PolicyHelper.GetHandler<string>(logger);
-        _getItemsHandler = PolicyHelper.GetHandler<IReadOnlyCollection<string>>(logger);
-        _getUserFullNameByIdHandler = PolicyHelper.GetHandler<OneOf<string, NotFound, Error>>(logger);
+        var getTimeAsStringHandler = PolicyHelper.GetHandler<string>(logger);
+        var getItemsHandler = PolicyHelper.GetHandler<IReadOnlyCollection<string>>(logger);
+        var getUserFullNameByIdHandler = PolicyHelper.GetHandler<OneOf<string, NotFound, Error>>(logger);
         if (configuration is null)
         {
-            _getTimeAsStringPolicy = PolicyHelper.GetRetryAndCircuitBreakerExceptionAsyncPolicySimple<string, HttpRequestException>();
-            _getItemsPolicy = PolicyHelper.GetRetryAndCircuitBreakerExceptionAsyncPolicySimple<IReadOnlyCollection<string>, HttpRequestException>();
-            _getUserFullNameByIdPolicy = PolicyHelper.GetRetryAndCircuitBreakerHttpRequestExceptionOrOneOfResultWithNotFoundAsyncPolicySimple<string>();
+            var getTimeAsStringPolicy = PolicyHelper.GetRetryAndCircuitBreakerExceptionAsyncPolicySimple<string, HttpRequestException>();
+            var getItemsPolicy = PolicyHelper.GetRetryAndCircuitBreakerExceptionAsyncPolicySimple<IReadOnlyCollection<string>, HttpRequestException>();
+            var getUserFullNameByIdPolicy = PolicyHelper.GetRetryAndCircuitBreakerHttpRequestExceptionOrOneOfResultWithNotFoundAsyncPolicySimple<string>();
+            _getTimeAsStringPolicyAndHandler = new PolicyAndHandlerWrapper<string>(getTimeAsStringPolicy, getTimeAsStringHandler);
+            _getItemsPolicyAndHandler = new PolicyAndHandlerWrapper<IReadOnlyCollection<string>>(getItemsPolicy, getItemsHandler);
+            _getUserFullNameByIdPolicyAndHandler = new PolicyAndHandlerWrapper<OneOf<string, NotFound, Error>>(getUserFullNameByIdPolicy, getUserFullNameByIdHandler);
         }
         else
         {
-            _getTimeAsStringPolicy = PolicyHelper.GetRetryAndCircuitBreakerExceptionAsyncPolicy<string, HttpRequestException>(configuration);
-            _getItemsPolicy = PolicyHelper.GetRetryAndCircuitBreakerExceptionAsyncPolicy<IReadOnlyCollection<string>, HttpRequestException>(configuration);
-            _getUserFullNameByIdPolicy = PolicyHelper.GetRetryAndCircuitBreakerHttpRequestExceptionOrOneOfResultWithNotFoundAsyncPolicy<string>(configuration);
+            var getTimeAsStringPolicy = PolicyHelper.GetRetryAndCircuitBreakerExceptionAsyncPolicy<string, HttpRequestException>(configuration);
+            var getItemsPolicy = PolicyHelper.GetRetryAndCircuitBreakerExceptionAsyncPolicy<IReadOnlyCollection<string>, HttpRequestException>(configuration);
+            var getUserFullNameByIdPolicy = PolicyHelper.GetRetryAndCircuitBreakerHttpRequestExceptionOrOneOfResultWithNotFoundAsyncPolicy<string>(configuration);
+            _getTimeAsStringPolicyAndHandler = new PolicyAndHandlerWrapper<string>(getTimeAsStringPolicy, getTimeAsStringHandler);
+            _getItemsPolicyAndHandler = new PolicyAndHandlerWrapper<IReadOnlyCollection<string>>(getItemsPolicy, getItemsHandler);
+            _getUserFullNameByIdPolicyAndHandler = new PolicyAndHandlerWrapper<OneOf<string, NotFound, Error>>(getUserFullNameByIdPolicy, getUserFullNameByIdHandler);
         }
     }
 
-    public Task<string> GetTimeAsString(bool fail, CancellationToken cancellationToken) => 
-        _getTimeAsStringPolicy.ExecuteAsync((fail, _api), static (p, token) => p._api.GetTimeAsString(p.fail, token), _getTimeAsStringHandler, cancellationToken);
+    public Task<string> GetTimeAsString(bool fail, CancellationToken cancellationToken) =>
+        _getTimeAsStringPolicyAndHandler.ExecuteAsync((fail, _api), static (p, token) => p._api.GetTimeAsString(p.fail, token), cancellationToken);
 
     public Task<IReadOnlyCollection<string>> GetItems(bool fail, CancellationToken cancellationToken) =>
-        _getItemsPolicy.ExecuteAsync((fail, _api), static (p, token) => p._api.GetItems(p.fail, token), _getItemsHandler, cancellationToken);
+        _getItemsPolicyAndHandler.ExecuteAsync((fail, _api), static (p, token) => p._api.GetItems(p.fail, token), cancellationToken);
 
     public async Task<OneOf<string, NotFound, Error>> GetUserFullNameById(int id, bool fail, CancellationToken cancellationToken)
     {
-        var result = await _getUserFullNameByIdPolicy.ExecuteAndCaptureAsync((fail, _api, id),
-            static (p, token) => p._api.GetUserFullNameById(p.id, p.fail, token), _getUserFullNameByIdHandler, cancellationToken);
+        var result = await _getUserFullNameByIdPolicyAndHandler.ExecuteAndCaptureAsync((fail, _api, id),
+            static (p, token) => p._api.GetUserFullNameById(p.id, p.fail, token), cancellationToken);
 
         return result.HandleFailureAndSuccess();
     }
