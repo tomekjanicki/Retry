353ec2efa7be4d5c3ab674608b09b7808503d001
Tomasz Janicki <tomasz.janicki@inetum.com>
2023-10-10 14:21:33 +0200

    refactor

--- Retry/Extensions/LoggerExtensions.cs
+++ Retry/Extensions/LoggerExtensions.cs
@@ -4,12 +4,20 @@ namespace Retry.Extensions;
 
 public static class LoggerExtensions
 {
-    public static void RetryLogResult<TResult>(this ILogger logger, Outcome<TResult> outcome)
+    public static void RetryLogResult<TResult>(this ILogger logger, Outcome<TResult> outcome) =>
+        LogResult(logger, outcome, static (logger, exception) => logger.LogError(exception, "Exception during retry."),
+            static (logger, result) => logger.LogError("Error during retry. {Result}", result));
+
+    public static void OpenLogResult<TResult>(this ILogger logger, Outcome<TResult> outcome) =>
+        LogResult(logger, outcome, static (logger, exception) => logger.LogError(exception, "Exception during open."),
+            static (logger, result) => logger.LogError("Error during open. {Result}", result));
+
+    private static void LogResult<TResult>(this ILogger logger, Outcome<TResult> outcome, Action<ILogger, Exception> exceptionAction, Action<ILogger, TResult> resultAction)
     {
         var exception = outcome.Exception;
         if (exception is not null)
         {
-            logger.LogError(exception, "Exception during retry.");
+            exceptionAction(logger, exception);
 
             return;
         }
@@ -18,23 +26,6 @@ public static class LoggerExtensions
         {
             return;
         }
-        logger.LogError("Error during retry. {Result}", result);
-    }
-
-    public static void OpenLogResult<TResult>(this ILogger logger, Outcome<TResult> outcome)
-    {
-        var exception = outcome.Exception;
-        if (exception is not null)
-        {
-            logger.LogError(exception, "Exception during open.");
-
-            return;
-        }
-        var result = outcome.Result;
-        if (result is null)
-        {
-            return;
-        }
-        logger.LogError("Error during open. {Result}", result);
+        resultAction(logger, result);
     }
 }
